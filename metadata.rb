de77761f66a8b1b3156984f121c1fe20b7cb2abf
Trevor Bramwell <bramwelt@osuosl.org>
2013-10-22 11:45:07 -0700

    Add Apache 2.0 LICENSE file and headers.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'base'
 maintainer       'OSU Open Source Lab'
 maintainer_email 'rudy@osuosl.org'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures base packages'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
