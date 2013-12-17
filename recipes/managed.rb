b6854dd2e0240e3f2b3af2f59c32afce22238c8d
Daniel Takamori <pono@osuosl.org>
2013-12-17 09:57:14 -0800

    comment out rsyslog defaults temp

--- recipes/managed.rb
+++ recipes/managed.rb
@@ -75,9 +75,9 @@ node.override['ldap']['uri'] = "ldaps://ldap1.osuosl.org/ ldaps://ldap2.osuosl.o
 node.override['ldap']['base'] = "dc=osuosl,dc=org"
 
 # Rsyslog configuration
-node.default['rsyslog']['server_search'] = "role:logstash_server"
-node.default['rsyslog']['port'] = "5000"
-node.default['rsyslog']['preserve_fqdn'] = "on"
+#node.default['rsyslog']['server_search'] = "role:logstash_server"
+#node.default['rsyslog']['port'] = "5000"
+#node.default['rsyslog']['preserve_fqdn'] = "on"
 
 # Nagios backend IP configuration
 mon_host = ['127.0.0.1']
