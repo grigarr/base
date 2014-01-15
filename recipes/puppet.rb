cb40195440b5583cadfcab0a2850f0cb03466602
Rudy Grigar <rudy@grigar.net>
2014-01-15 15:02:15 -0800

    use the yum v2 cookbook syntax for lwrp

--- recipes/puppet.rb
+++ recipes/puppet.rb
@@ -3,10 +3,10 @@ yum_repository "puppetlabs" do
   repo_name "puppetlabs"
   description "puppetlabs repo $releasever - $basearch"
   url "http://yum.puppetlabs.com/el/6/products/$basearch"
-  gpgkey "https://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs"
   action :create
 end
 
-package "puppet" do
-  action :install
+yum_key "RPM-GPG-KEY-puppetlabs" do
+  url "https://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs"
+  action :add
 end
